ede6e7d67bdb837f9416189794159f2fa86c2870
abhiram <abhiramsankarsetty@gmail.com>
2025-02-19 22:35:16 +0530

    made some styling changes using tailwind css

--- src/components/ModelMessage.jsx
+++ src/components/ModelMessage.jsx
@@ -4,7 +4,7 @@ import Markdown from 'react-markdown'
 const ModelMessage = ({ message }) => {
     return (
       <div className="flex justify-start my-2">
-        <div className="bg-gray-200 text-black p-3 rounded-2xl max-w-[75%] shadow-md">
+        <div className="bg-[#25282A] text-white p-3 rounded-2xl max-w-[75%] shadow-md">
           <Markdown>{message}</Markdown>
         </div>
       </div>
